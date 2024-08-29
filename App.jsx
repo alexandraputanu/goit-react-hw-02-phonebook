b6b420720aeb9548b352144551058b76bfc72556
Alexandra Putanu <alexandra_putanu@yahoo.com>
2024-08-29 21:11:28 +0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import Example from "./components/exemple";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Example /> {/* Componenta Example, fără alte stilizări în App.jsx */}
+    </div>
+  );
+}
+
+export default App;
---
+++ exemple.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const Example = () => {
+  return (
+    <div className="bg-gray-100 p-8 rounded-lg shadow-lg">
+      <h1 className="text-4xl font-bold text-blue-600 p-10">Bine ai venit!</h1>
+      <p className="text-lg text-gray-700 mt-4">
+        Aceasta este componenta Example stilizată complet cu Tailwind CSS.
+        Stilurile, sunt aplicate în această componentă.
+      </p>
+    </div>
+  );
+};
+
+export default Example;
